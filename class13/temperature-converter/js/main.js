ca3800634991867c3c94be0207ba22416c928f2c
gensai-sec <childresszac@gmail.com>
2024-02-15 20:38:34 -0600

    Classes 12 - 14 work

--- class13/temperature-converter/js/main.js
+++ class13/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+
+document.querySelector('#sel').addEventListener('click', convert);
+
+function convert() {
+    //need val in C
+    let temp = document.querySelector('#celsius').value;
+    //convert from C to F
+    temp = temp * 9/5 + 32;
+    //show it
+    document.querySelector('#ctof').innerText = temp;
+}
